077cc3bbec0c7332d3ec46518eb2a3dfe6c21889
Y/Groot The AW <32866441+Andrew0079@users.noreply.github.com>
2020-07-19 12:19:47 +0100

    base app

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/andrasstrublics/Documents/Code_Repo/PYTHON/Django/django_chat/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
